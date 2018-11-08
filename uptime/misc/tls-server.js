999a5a0cd97fb517396111b0fa53351dcf2403a4
Josh Loftis <jshlfts32@gmail.com>
2018-11-08 18:13:17 -0500

    Add tls example

---
+++ uptime/misc/tls-server.js
@@ -0,0 +1,32 @@
+/*
+* Example TCP (tls) Server
+* Listens to port 6000 and sends the word "pong" to client
+*
+*/
+
+// Dependecies
+const tls = require('tls');
+const fs = require('fs');
+const path = require('path');
+
+// Server options
+const options = {
+  'key': fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
+  'cert': fs.readFileSync(path.join(__dirname, '/../https/cert.pem'))
+};
+
+// Create the server
+const server = tls.createServer(options, function(connection) {
+  // Send the word "pong"
+  const outboundMessage = "pong";
+  connection.write(outboundMessage);
+
+  // When the client writes something, log it out
+  connection.on('data', function(inboundMessage) {
+    const messageString = inboundMessage.toString();
+    console.log(`I wrote ${outboundMessage} and they said ${messageString}`);
+  });
+});
+
+// Listen
+server.listen(6000);
