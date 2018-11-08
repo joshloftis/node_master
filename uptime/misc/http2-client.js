5111d0097f3b6e51b0a0a59529f9ed023d528d30
Josh Loftis <jshlfts32@gmail.com>
2018-11-08 17:45:34 -0500

    Http2 example

---
+++ uptime/misc/http2-client.js
@@ -0,0 +1,30 @@
+/*
+* Example HTTP2 Client
+*
+*
+*/
+
+// Dependecies
+ const http2 = require('http2');
+
+// Create client
+const client = http2.connect('http://localhost:6000');
+
+// Create a request
+const req = client.request({
+  ':path' : '/'
+});
+
+// When a message is received, add the pieces of it together until you reach the end
+let str = '';
+req.on('data', function(chunk) {
+  str += chunk;
+});
+
+// When a message ends, log it out
+req.on('end', function() {
+  console.log(str);
+});
+
+// End the request
+req.end();
